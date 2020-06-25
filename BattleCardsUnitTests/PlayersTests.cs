45cc7d3dd0a8958923a58f816145f973cc259f67
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-06-25 16:25:09 +0200

    Add PlayersTsts

--- BattleCardsUnitTests/PlayersTests.cs
+++ BattleCardsUnitTests/PlayersTests.cs
@@ -6,52 +6,81 @@ namespace BattleCardsUnitTests
 {
     public class PlayersTests
     {
+        private Player _playerWithOneCard;
+        private Player _playerWithThreeCards;
+        private Player _playerWithNoCards;
+        private Player _playerAiWithOneCard;
+        
         [SetUp]
         public void Setup()
         {
+            _playerWithOneCard = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
+            _playerWithNoCards = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
+            _playerWithThreeCards = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
+            _playerAiWithOneCard= new Player(new AiCategorySelector()) {Deck = new Queue<Card>()};
+            
+            _playerWithOneCard.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
+            _playerAiWithOneCard.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
+            
+            _playerWithThreeCards.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
+            _playerWithThreeCards.Deck.Enqueue(new Card("Second", "1", "2", "3", "4"));
+            _playerWithThreeCards.Deck.Enqueue(new Card("Third", "1", "2", "3", "4"));
         }
-        
+        [TearDown]
+        public void TearDown()
+        {
+            _playerWithNoCards = null;
+            _playerWithOneCard = null;
+            _playerWithThreeCards = null;
+            _playerAiWithOneCard = null;
+
+        }
         [Test]
         public void IsRemovePlayersWithoutCardsRemovePLayersWithNoCards()
         {
-            var player1 = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
-            var player2 = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
-            
-            player1.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
-            
             var players = new Players(new List<Player>
             {
-                player1, player2
+                _playerWithNoCards,
+                _playerWithOneCard,
+                _playerAiWithOneCard
             });
-            
             players.RemovePlayersWithoutCards();
             
-            Assert.AreEqual(1, players.PlayersList.Count);
-            
+            Assert.AreEqual(2, players.PlayersList.Count);
         }
+
         [Test]
-        public void IsRemovePlayersWithoutCardsLeavePlayersWithCards()
+        [Ignore("Function not implemented")]
+        public void SwapActivePlayerChangeActivePlayerToNextInList()
         {
-            var player1 = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
-            var player2 = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
-            var player3 = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
-            var player4 = new Player(new HumanCategorySelector()) {Deck = new Queue<Card>()};
-            
-            player1.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
-            player2.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
-            player3.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
-            player4.Deck.Enqueue(new Card("First", "1", "2", "3", "4"));
-            
             var players = new Players(new List<Player>
             {
-                player1, player2, player3, player4
+                _playerWithNoCards,
+                _playerWithOneCard,
+                _playerAiWithOneCard
             });
+            players.ActivePlayer = _playerWithNoCards;
             
-            players.RemovePlayersWithoutCards();
+            players.SwapActivePlayer();
             
-            Assert.AreEqual(4, players.PlayersList.Count);
-            
+            Assert.AreEqual(_playerWithOneCard, players.ActivePlayer );
         }
+        
+        [Test]
+        public void GiveFirstCad()
+        {
+            var players = new Players(new List<Player>
+            {
+                _playerWithOneCard,
+                _playerAiWithOneCard,
+                _playerWithThreeCards,
+            });
+            var result = players.GiveFirstCard();
+
+            Assert.AreEqual(0, _playerWithOneCard.Deck.Count);
+            Assert.AreEqual(3, result.Count);
+        }
+        
       
     }
 }
